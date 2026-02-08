a2e1b6ed62227f91b69ed7d922aeb7b16530ef6d
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2026-02-08 20:16:32 +0530

    Add files via upload

--- coverup.sh
+++ coverup.sh
@@ -15,27 +15,39 @@ cd ../..
 rm -rvf $coverup_tmp/$task_name
 mkdir -p $coverup_tmp/$task_name
 
-tar -cf $coverup_tmp/$task_name/$task_name.tar.xz $task_name
+
+
+timex="$(date '+%Y_%m_%d_%H%M')"
+task_fname="$task_name-$timex.tar.xz"
+
+echo "tarball folder"
+tar -cvf $coverup_tmp/$task_name/$task_fname $task_name
 
 echo "Cover up done .."
 
 cd $coverup_tmp/$task_name
 
 
-acrypter encrypt cry15360 $task_name.tar.xz $task_name.tar.xz.acrypt
-rm $task_name.tar.xz
+
+acrypter encrypt cry15360 $task_fname $task_fname.acrypt
+echo "Encryption Done"
+if [ -f $task_name.tar.xz ]
+then
+	rm $task_name.tar.xz
+fi
 
 if [ -z "${2+x}" ]; then
   echo " $task_name done over single file"
 else
-  acrypter split $task_name.tar.xz.acrypt $2 $task_name.tar.xz.acrypt
-  rm $task_name.tar.xz.acrypt
+  acrypter split $task_fname.acrypt $2 $task_fname.acrypt
+  echo "Splitting Done"
+  rm $task_fname
 fi
 
 
 cd ../../..
 
-rm -rvf $coverup_release/$task_name
+rm -rf $coverup_release/$task_name
 mkdir -p $coverup_release
 
 mv $coverup_tmp/$task_name $coverup_release/$task_name
